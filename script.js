2092ccd9caa07e433d691088506f40980395a6a3
Vevest <vvevest@gmail.com>
2024-04-26 18:15:12 +0200

    javascript

---
+++ script.js
@@ -0,0 +1,60 @@
+//Definerer mine variabler
+var scenarie = document.getElementById('scenarie');
+var btn1 = document.getElementById('btn1');
+var screen1 = document.getElementById('screen1');
+var btn2 = document.getElementById('btn2');
+var screen2 = document.getElementById('screen2');
+var btn3 = document.getElementById('btn3');
+var screen3 = document.getElementById('screen3');
+var btn4 = document.getElementById('btn4');
+var screen2a = document.getElementById('screen2a');
+var btn5 = document.getElementById('btn5');
+var screen2b = document.getElementById('screen2b');
+var btn6 = document.getElementById('btn6');
+var screen3a = document.getElementById('screen3a');
+var btn7 = document.getElementById('btn7');
+var screen3b = document.getElementById('screen3b');
+var btn8 = document.getElementById('btn8');
+var screen3c = document.getElementById('screen3c');
+var btn9 = document.getElementById('btn9');
+
+// Skjul alle skærme undtagen scenarie ved start af testen
+hideAllScreens();
+scenarie.classList.remove('hide');
+
+// Funktion til at skjule alle skærme
+function hideAllScreens() {
+    if (screen1) screen1.classList.add('hide');
+    if (screen2) screen2.classList.add('hide');
+    if (screen3) screen3.classList.add('hide');
+    if (screen2a) screen2a.classList.add('hide');
+    if (screen2b) screen2b.classList.add('hide');
+    if (screen3a) screen3a.classList.add('hide');
+    if (screen3b) screen3b.classList.add('hide');
+    if (screen3c) screen3c.classList.add('hide');
+}
+
+
+btn1.addEventListener('click', function() {
+    // Tjek hvilken svarmulighed der er valgt
+    var selectedAnswer = document.querySelector('input[name="answer"]:checked');
+
+    if (selectedAnswer) {
+        console.log('Valgt svarmulighed:', selectedAnswer.value);
+
+        // Skift til næste skærm baseret på valgt svarmulighed
+        if (selectedAnswer.value === 'svarA') {
+            scenarie.classList.add('hide');
+            screen1.classList.remove('hide');
+        } else if (selectedAnswer.value === 'svarB') {
+            scenarie.classList.add('hide');
+            screen2.classList.remove('hide');
+        } else if (selectedAnswer.value === 'svarC') {
+            scenarie.classList.add('hide');
+            screen3.classList.remove('hide');
+        }
+        
+    } else {
+        alert('Vælg venligst et svar for at fortsætte.');
+    }
+});
